abb7719351c7c5b776e8c67a7f7ba66bcdd8b319
Bibek Giri <giribibek01@gmail.com>
2026-07-28 09:16:59 +0545

    Add OnboardingScreen with user input fields for shopping preferences and age range

---
+++ lib/screens/home/onboarding_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 180),
+
+              Text(
+                "Tell us About yourself",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+              ),
+
+              SizedBox(height: 40),
+
+              Text(
+                "Who do you shop for ?",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+
+              SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 60,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF8E6CEF),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Men",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    height: 60,
+                    width: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withValues(alpha: 0.55),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Women",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 40),
+
+              Text(
+                "How Old are you ?",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+
+              SizedBox(height: 20),
+
+              TextField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.arrow_drop_down),
+                  filled: true,
+                  fillColor: Colors.grey.withValues(alpha: 0.15),
+                  hintText: "Age Range",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
