433f712b983af6783a1796e28a392a1337ddff60
Bibek Giri <giribibek01@gmail.com>
2026-07-28 09:15:23 +0545

    Add SigninScreen with password input and navigation to onboarding and forgot password screens

---
+++ lib/screens/auth/signin_screen.dart
@@ -0,0 +1,97 @@
+import 'package:ecommerce/screens/auth/forgot_pswd_screen.dart';
+import 'package:ecommerce/screens/home/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatelessWidget {
+  const SigninScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 160),
+              Text(
+                "Sign In",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+              ),
+
+              SizedBox(height: 20),
+
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.withValues(alpha: 0.15),
+                  hintText: "Password",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => OnboardingScreen()),
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF8E6CEF),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Continue",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 15),
+
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => ForgotPswdScreen()),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Forgot password?",
+                    style: TextStyle(fontSize: 13, color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: " Reset",
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
