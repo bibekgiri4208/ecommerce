e2edb2a4a6a43c5ebd3d01dcf46ca9641b082f9f
Bibek Giri <giribibek01@gmail.com>
2026-07-28 09:15:07 +0545

    Refactor AuthScreen layout and navigation for improved user experience

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/screens/auth/create_account_screen.dart';
+import 'package:ecommerce/screens/auth/signin_screen.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -8,133 +10,166 @@ class AuthScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 120),
-            Text(
-              "Sign In",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
-            ),
-
-            SizedBox(height: 20),
-
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey.withValues(alpha: 0.15),
-                hintText: "Email Address",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(5),
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 160),
+              Text(
+                "Sign In",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
               ),
-            ),
 
-            SizedBox(height: 20),
+              SizedBox(height: 20),
 
-            Container(
-              height: 60,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Color(0xFF8E6CEF),
-                borderRadius: BorderRadius.circular(40),
-              ),
-              child: Center(
-                child: Text(
-                  "Continue",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.withValues(alpha: 0.15),
+                  hintText: "Email Address",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(5),
                   ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 15),
+              SizedBox(height: 20),
 
-            RichText(
-              text: TextSpan(
-                text: "Don't have an Account?",
-                style: TextStyle(fontSize: 13, color: Colors.black),
-                children: [
-                  TextSpan(
-                    text: " Create One",
-                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => SigninScreen()),
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF8E6CEF),
+                    borderRadius: BorderRadius.circular(40),
                   ),
-                ],
+                  child: Center(
+                    child: Text(
+                      "Continue",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
 
-            SizedBox(height: 40),
+              SizedBox(height: 15),
 
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 60,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(40),
-              ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Text(
-                    "Continue with Apple",
-                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => CreateAccountScreen()),
+                  );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: "Don't have an Account?",
+                    style: TextStyle(fontSize: 13, color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: " Create One",
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
                   ),
-                  Positioned(
-                    left: 20,
-                    child: Image.asset("assets/apple_icon.png"),
-                  ),
-                ],
+                ),
               ),
-            ),
 
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 60,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(40),
-              ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Text(
-                    "Continue with Google",
-                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
-                  ),
-                  Positioned(left: 20, child: Image.asset("assets/google.png")),
-                ],
-              ),
-            ),
+              SizedBox(height: 40),
 
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              height: 60,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(40),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                height: 60,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Text(
+                      "Continue with Apple",
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Positioned(
+                      left: 20,
+                      child: Image.asset("assets/apple_icon.png"),
+                    ),
+                  ],
+                ),
               ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Text(
-                    "Continue with Facebook",
-                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500),
-                  ),
-                  Positioned(
-                    left: 20,
-                    child: Image.asset("assets/facebook.png"),
-                  ),
-                ],
+
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                height: 60,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Text(
+                      "Continue with Google",
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Positioned(
+                      left: 20,
+                      child: Image.asset("assets/google.png"),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                height: 60,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Text(
+                      "Continue with Facebook",
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Positioned(
+                      left: 20,
+                      child: Image.asset("assets/facebook.png"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
