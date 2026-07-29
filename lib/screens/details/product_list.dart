4e7f5caf2a14b26c61ae4c663b8f2f72147582e6
Bibek Giri <giribibek01@gmail.com>
2026-07-29 09:36:28 +0545

    Add product list with images for accessories, bags, hoodies, shoes, and shorts

---
+++ lib/screens/details/product_list.dart
@@ -0,0 +1,7 @@
+List productData = [
+  {'name': "Hoodies", 'image': "assets/hoodies.png"},
+  {'name': "Shorts", 'image': "assets/shorts.png"},
+  {'name': "Shoes", 'image': "assets/shoes.png"},
+  {'name': "Bag", 'image': "assets/bag.png"},
+  {'name': "Accessories", 'image': "assets/accessories.png"},
+];
