d9f0dcd16d84b6a918983f537f852ad9ae73261f
Bibek Giri <giribibek01@gmail.com>
2026-07-30 09:27:58 +0545

    Refactor ProfileScreen layout for improved readability and structure

--- lib/screens/home/profile_screen.dart
+++ lib/screens/home/profile_screen.dart
@@ -10,203 +10,216 @@ class ProfileScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(height: 80),
-            Center(
-              child: CircleAvatar(
-                radius: 60,
-                backgroundImage: AssetImage("assets/men.png"),
-              ),
-            ),
-
-            SizedBox(height: 50),
-
-            Container(
-              height: 120,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 20,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 80),
+              Center(
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage("assets/men.png"),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              ),
 
-                      children: [
-                        Text(
-                          "Bibek Giri",
-                          style: GoogleFonts.gabarito(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
+              SizedBox(height: 50),
+
+              Container(
+                height: 120,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text(
+                            "Bibek Giri",
+                            style: GoogleFonts.gabarito(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
+                          Text(
+                            "giribibek@gmail.com",
+                            style: GoogleFonts.gabarito(color: Colors.grey),
+                          ),
+                          Text(
+                            "123-456-789",
+                            style: GoogleFonts.gabarito(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Edit",
+                        style: GoogleFonts.gabarito(
+                          color: Colors.deepPurpleAccent,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 15,
                         ),
-                        Text(
-                          "giribibek@gmail.com",
-                          style: GoogleFonts.gabarito(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Container(
+                height: 65,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Address",
+                        style: GoogleFonts.gabarito(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
                         ),
-                        Text(
-                          "123-456-789",
-                          style: GoogleFonts.gabarito(color: Colors.grey),
+                      ),
+                      Icon(Icons.keyboard_arrow_right_outlined),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Container(
+                height: 65,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Wishlist",
+                        style: GoogleFonts.gabarito(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
                         ),
-                      ],
-                    ),
-                    Text(
-                      "Edit",
-                      style: GoogleFonts.gabarito(
-                        color: Colors.deepPurpleAccent,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 15,
                       ),
-                    ),
-                  ],
+                      Icon(Icons.keyboard_arrow_right_outlined),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 20),
+              SizedBox(height: 10),
 
-            Container(
-              height: 65,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Address",
-                      style: GoogleFonts.gabarito(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
+              Container(
+                height: 65,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Payment",
+                        style: GoogleFonts.gabarito(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
+                        ),
                       ),
-                    ),
-                    Icon(Icons.keyboard_arrow_right_outlined),
-                  ],
+                      Icon(Icons.keyboard_arrow_right_outlined),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 10),
+              SizedBox(height: 10),
 
-            Container(
-              height: 65,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Wishlist",
-                      style: GoogleFonts.gabarito(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
+              Container(
+                height: 65,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Help",
+                        style: GoogleFonts.gabarito(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
+                        ),
                       ),
-                    ),
-                    Icon(Icons.keyboard_arrow_right_outlined),
-                  ],
+                      Icon(Icons.keyboard_arrow_right_outlined),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 10),
+              SizedBox(height: 10),
 
-            Container(
-              height: 65,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Payment",
-                      style: GoogleFonts.gabarito(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
+              Container(
+                height: 65,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withValues(alpha: 0.15),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Support",
+                        style: GoogleFonts.gabarito(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w200,
+                        ),
                       ),
-                    ),
-                    Icon(Icons.keyboard_arrow_right_outlined),
-                  ],
+                      Icon(Icons.keyboard_arrow_right_outlined),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 10),
+              SizedBox(height: 20),
 
-            Container(
-              height: 65,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Help",
-                      style: GoogleFonts.gabarito(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ),
-                    Icon(Icons.keyboard_arrow_right_outlined),
-                  ],
+              Text(
+                "Sign Out",
+                style: GoogleFonts.gabarito(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
                 ),
               ),
-            ),
-
-            SizedBox(height: 10),
-
-            Container(
-              height: 65,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.15),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Support",
-                      style: GoogleFonts.gabarito(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ),
-                    Icon(Icons.keyboard_arrow_right_outlined),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
