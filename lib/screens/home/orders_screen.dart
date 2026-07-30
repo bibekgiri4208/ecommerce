00509eb8081f2e40fc91d1b2fc162004f7fafe19
Bibek Giri <giribibek01@gmail.com>
2026-07-30 08:27:32 +0545

    Add OrdersScreen with UI elements and image for order status

---
+++ lib/screens/home/orders_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Orders",
+          style: GoogleFonts.gabarito(
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/orders_img.png", height: 120, width: 120),
+
+              SizedBox(height: 20),
+
+              Text("No Orders yet", style: GoogleFonts.gabarito(fontSize: 26)),
+
+              SizedBox(height: 30),
+
+              Container(
+                height: 60,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Color(0xFF8E6CEF),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Center(
+                  child: Text(
+                    "Explore Categories",
+                    style: GoogleFonts.gabarito(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
